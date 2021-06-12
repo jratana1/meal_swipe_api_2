5dccc5e05f8badff400dbc711d39b413b29449b8
John Ratana <jratana1@gmail.com>
2021-06-11 21:39:20 -0400

    update

--- app/models/Yelp_Api_Adaptor.rb
+++ app/models/Yelp_Api_Adaptor.rb
@@ -17,31 +17,5 @@ class YelpApiAdaptor < ApplicationRecord
       response.parse
     end
     
-    def self.yelp_rest_hash_converter(hash)
-        rest_hash = {}
-        rest_hash[:name] = hash["name"]
-        rest_hash[:address] = hash["location"]["address1"]
-        rest_hash[:city] = hash["location"]["city"]
-        rest_hash[:state] = hash["location"]["state"]
-        rest_hash[:postal_code] = hash["location"]["postal_code"]
-        rest_hash[:yelp_id] = hash["id"]
-        rest_hash[:photos] = hash["photos"][0]
-        rest_hash
-    end
-
-    def self.yelp_cat_hash_converter(hash)
-      @category_array = hash["categories"]
-    end
-
-    def self.make_restauarant(restaurant)
-        @rest_hash = YelpApiAdaptor.yelp_rest_hash_converter(restaurant)
-        @cat_hash = YelpApiAdaptor.yelp_cat_hash_converter(restaurant)    
-        if @rest_hash[:image_url] && !Restaurant.find_by_yelp_id(@rest_hash[:yelp_id])
-          restaurant = Restaurant.create(@rest_hash)            
-          restaurant.photos << Photo.create(url:restaurant.image_url)          
-          @cat_hash.each do |hash|
-            restaurant.categories << Category.create_with(hash).find_or_create_by(title: hash["title"])
-            end
-        end
-    end      
+   
 end
