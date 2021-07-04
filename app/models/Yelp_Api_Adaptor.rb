4c0d2c99cf921f61bd8fb8032f897a66902c9bff
John Ratana <jratana1@gmail.com>
2021-07-04 11:32:49 -0400

    done

--- app/models/Yelp_Api_Adaptor.rb
+++ app/models/Yelp_Api_Adaptor.rb
@@ -2,11 +2,11 @@ class YelpApiAdaptor < ApplicationRecord
     API_HOST = "https://api.yelp.com"
     SEARCH_PATH = "/v3/businesses/search"
     BUSINESS_PATH = "/v3/businesses/"
-    SEARCH_LIMIT = 50
+    SEARCH_LIMIT = 10
     API_URL = "https://api.yelp.com/v3/graphql"
     
     def self.api_search(location, categories = "restaurants", offset = 1)
-      body = "{search(term: \"food\", location: \"#{location}\", categories: \"#{categories}\", limit: 50, offset: #{offset}) {business {id, name, location{address1, city, state, postal_code}, photos}}}"
+      body = "{search(term: \"food\", location: \"#{location}\", categories: \"#{categories}\", limit: 10, offset: #{offset}) {business {id, name, location{address1, city, state, postal_code}, photos}}}"
       response = HTTP.auth("Bearer #{ENV['API_KEY']}").headers("Content-Type" => "application/graphql").post("https://api.yelp.com/v3/graphql", :body => body)
       response.parse["data"]["search"]["business"]
     end
