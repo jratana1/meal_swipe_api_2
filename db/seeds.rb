78199143fb8f5962a73efded176a1fd6538b86c3
John Ratana <jratana1@gmail.com>
2021-09-10 15:47:35 -0400

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+require "securerandom"
+
+# generate 20 users
+(1..20).each do |id|
+    User.create!(
+# each user is assigned an id from 1-20
+        name: Faker::Name.name,
+        email: Faker::Internet.email,
+        provider: "google",
+        uid: SecureRandom.uuid
+    )
+end
